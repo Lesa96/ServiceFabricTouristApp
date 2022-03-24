aae18c08d40b9ce8ce03ed627f7013e1efdb53ce
Sasa Dragomirovic <Sasa.Dragomirovic@bimexperts.com>
2022-03-24 09:43:30 +0100

    Added stateless client

---
+++ TouristApp/Client/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        //http://localhost:9031/home/get
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
